a1d8eea626a8b389066d94f60928996448273ca6
Shiyu Liu <jamessliu2020@gmail.com>
2021-12-13 20:54:36 -0600

    added build version

---
+++ build/precache-manifest.e93eb16f4c6df2f014d84c104f900c52.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ae39574f7fcdcf599083e1759f59510",
+    "url": "/index.html"
+  },
+  {
+    "revision": "365e271db05cfd61ede4",
+    "url": "/static/css/main.4f269277.chunk.css"
+  },
+  {
+    "revision": "a3d57d5af8527953ccde",
+    "url": "/static/js/2.13cb28ff.chunk.js"
+  },
+  {
+    "revision": "424d73837ffd7843aa9380b24eb738f8",
+    "url": "/static/js/2.13cb28ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "365e271db05cfd61ede4",
+    "url": "/static/js/main.1efcab09.chunk.js"
+  },
+  {
+    "revision": "693d8af27c1684c7fee9",
+    "url": "/static/js/runtime-main.20628b47.js"
+  }
+]);
